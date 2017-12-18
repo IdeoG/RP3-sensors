935139b54b060035ebfcc293158e751724035b4f
Ideo G <ideogaib@gmail.com>
2017-12-19 00:38:14 +0300

    acs712 3.3V logic

--- acs712/example.py
+++ acs712/example.py
@@ -4,12 +4,12 @@ import Adafruit_ADS1x15
 if __name__ == "__main__":
     adc = Adafruit_ADS1x15.ADS1115()
     GAIN = 1
-    v_ref = 5
-    k_c = 50
-    k_v = 4.096 / float(1<<15)
+    V_REF = 3.2
+    KC =  5 / (V_REF*0.1)
+    KV = 4.096 / float(1<<15)
     
     while True:
-        adc_value = adc.read_adc(1, gain=GAIN) * k_v
-        current = adc_value - v_ref/2
+        adc_value = adc.read_adc(0, gain=GAIN) * KV
+        current = (adc_value - V_REF/2) * KC
         print("current: {}, adc_value: {}".format(current, adc_value))
         time.sleep(1)
