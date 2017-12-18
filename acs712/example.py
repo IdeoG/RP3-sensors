5915d26a63311e91fded4dafcfe094009f88c2e1
Ideo G <ideogaib@gmail.com>
2017-12-19 00:44:54 +0300

    acs712 3.3V logic Vref calibrated

--- acs712/example.py
+++ acs712/example.py
@@ -4,12 +4,12 @@ import Adafruit_ADS1x15
 if __name__ == "__main__":
     adc = Adafruit_ADS1x15.ADS1115()
     GAIN = 1
-    V_REF = 3.2
+    V_REF = 3.214
     KC =  5 / (V_REF*0.1)
     KV = 4.096 / float(1<<15)
     
     while True:
         adc_value = adc.read_adc(0, gain=GAIN) * KV
-        current = (adc_value - V_REF/2) * KC
-        print("current: {}, adc_value: {}".format(current, adc_value))
+        current = round((adc_value - V_REF/2) * KC, 3)
+        print("current: {}A, adc_value: {}V".format(current, round(adc_value, 3)))
         time.sleep(1)
